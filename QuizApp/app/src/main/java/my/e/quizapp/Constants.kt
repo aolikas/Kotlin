8830619481f165d380c33fd6a7653e2ba42cd1f3
Aolika <olkaspark@gmail.com>
2021-11-02 17:21:43 +0100

    added UI

---
+++ QuizApp/app/src/main/java/my/e/quizapp/Constants.kt
@@ -0,0 +1,110 @@
+package my.e.quizapp
+
+import java.util.ArrayList
+
+object Constants {
+    fun getQuestions(): ArrayList<Question> {
+        val questionList = ArrayList<Question>()
+        val que1 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_argentina,
+            "Argentina", "Australia",
+            "Russia", "Armenia",
+            1
+        )
+        questionList.add(que1)
+
+        val que2 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_australia,
+            "Fiji", "Australia",
+            "New Zealand", "Brazil",
+            2
+        )
+
+        questionList.add(que2)
+
+        val que3 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_belgium,
+            "Australia", "Denmark",
+            "Germany", "Belgium",
+            4
+        )
+
+        questionList.add(que3)
+
+        val que4 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_brazil,
+            "Kuwait", "India",
+            "Chili", "Brazil",
+            4
+        )
+
+        questionList.add(que4)
+
+        val que5 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_new_zealand,
+            "England", "Australia",
+            "New Zealand", "Fiji",
+            3
+        )
+
+        questionList.add(que5)
+
+        val que6 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_kuwait,
+            "Oman", "Turkey",
+            "Kuwait", "India",
+            3
+        )
+
+        questionList.add(que6)
+
+        val que7 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_denmark,
+            "Denmark", "Finland",
+            "Norway", "Belgium",
+            2
+        )
+
+        questionList.add(que7)
+
+        val que8 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_fiji,
+            "Kuwait", "Fiji",
+            "New Zealand", "Brazil",
+            2
+        )
+
+        questionList.add(que8)
+
+        val que9 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_germany,
+            "France", "Sweden",
+            "Hungary", "Germany",
+            4
+        )
+
+        questionList.add(que9)
+
+        val que10 = Question(
+            1, "What country does this flag belong to?",
+            R.drawable.ic_flag_of_india,
+            "China", "Bhutan",
+            "India", "Iran",
+            3
+        )
+
+        questionList.add(que10)
+
+
+        return questionList
+    }
+}
--- QuizApp/app/src/main/java/my/e/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/my/e/quizapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package my.e.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val etName: EditText = findViewById(R.id.et_name)
+        val btnStart: Button = findViewById(R.id.btn_start)
+        btnStart.setOnClickListener {
+            if(etName.text.isEmpty()) {
+                Toast.makeText(this, "Enter a name", Toast.LENGTH_SHORT).show()
+            } else {
+                val intent = Intent(this, QuizQuestionActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+
+        }
     }
 }
