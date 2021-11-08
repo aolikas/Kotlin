f00afbeb757e77ad0be0741a05331b54f7602449
aolikas <olkaspark@gmail.com>
2021-11-08 20:22:45 +0100

    added a color selection

--- DrawingApp/app/src/main/java/my/e/drawingapp/MainActivity.kt
+++ DrawingApp/app/src/main/java/my/e/drawingapp/MainActivity.kt
@@ -3,8 +3,10 @@ package my.e.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import kotlinx.android.synthetic.main.dialog_brush_size.*
@@ -59,7 +61,19 @@ class MainActivity : AppCompatActivity() {
 
         brushDialog.show()
 
+    }
 
+    fun paintClicked(view: View) {
+    if(view!== mImageButtonCurrentPaint) {
+        val imageButton = view as ImageButton
+        val colorTag = imageButton.tag.toString()
+        drawingView?.setColor(colorTag)
+       imageButton.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed))
+        mImageButtonCurrentPaint?.setImageDrawable(
+            ContextCompat.getDrawable(this, R.drawable.pallet_normal))
 
+        mImageButtonCurrentPaint = view
+    }
     }
 }
